390ba6aae0523fa6bb48d970c59ee5309c32ec88
luannt1010 <luanntai13@gmail.com>
2026-06-21 17:51:45 +0700

    add update code create neck

--- src/neck.py
+++ src/neck.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from src.flow import FineFlow, ContextFlow
-from src.gate import CrossAttention, ScaleGate
+from src import FineFlow, ContextFlow
+from src import CrossAttention, ScaleGate
 
 
 class SDDFBNeck(nn.Module):
