c135ebae00c4b2172d6ed2914e1c0ce63c290424
luannt1010 <luanntai13@gmail.com>
2026-06-27 19:11:47 +0700

    add update fpn network

--- src/model.py
+++ src/model.py
@@ -1,16 +1,18 @@
 import torch.nn as nn
 
 class SDDFBModel(nn.Module):
-    def __init__(self, backbone, neck, head):
+    def __init__(self, backbone, fpn, neck, head):
         super().__init__()
 
         self.backbone = backbone
+        self.fpn = fpn
         self.neck = neck
         self.head = head
 
     def forward(self, images):
-        p2,p3,p4,p5 = self.backbone(images)
-        neck_out = self.neck(p2,p3,p4,p5)
+        c2, c3, c4, c5 = self.backbone(images)
+        p2, p3, p4, p5 = self.fpn(c2, c3, c4, c5)
+        neck_out = self.neck(p2, p3, p4, p5)
         try:
             outputs = self.head(neck_out)
         except TypeError:
