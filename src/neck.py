5e7627f0ad03684c74d98b5bdd575de059bc177f
luannt1010 <luanntai13@gmail.com>
2026-08-21 23:25:15 +0700

    add update neck

--- src/neck.py
+++ src/neck.py
@@ -5,21 +5,21 @@ from src import CrossAttention, ScaleGate
 
 
 class SDDFBNeck(nn.Module):
-    def __init__(self, c2, c3, c4, c5, out_channels=256, num_heads=8):
+    def __init__(self, c2, c3, c4, c5, out_channels=256, num_heads=16, window_size=64):
         super().__init__()
 
         self.fine_flow = FineFlow(c2, c3, out_channels)
         self.context_flow = ContextFlow(c4, c5, out_channels)
-        self.fine_to_context = CrossAttention(out_channels, num_heads)
-        self.context_to_fine = CrossAttention(out_channels, num_heads)
+        self.fine_to_context = CrossAttention(out_channels, num_heads, window_size)
+        self.context_to_fine = CrossAttention(out_channels, num_heads, window_size)
         self.scale_gate = ScaleGate(out_channels)
 
     def forward(self, p2, p3, p4, p5):
         fine_feat = self.fine_flow(p2, p3)
         context_feat = self.context_flow(p4, p5)
         context_feat = F.interpolate(context_feat, size=fine_feat.shape[-2:], mode="bilinear", align_corners=False)
-        fine_enhanced = (fine_feat + self.fine_to_context(fine_feat, context_feat))
-        context_enhanced = (context_feat + self.context_to_fine(context_feat, fine_feat))
+        fine_enhanced = self.fine_to_context(fine_feat, context_feat)
+        context_enhanced = self.context_to_fine(context_feat, fine_feat)
         fused, weights = self.scale_gate(fine_enhanced, context_enhanced)
 
         return {"fused": fused, "fine": fine_enhanced, "context": context_enhanced, "gate": weights}
